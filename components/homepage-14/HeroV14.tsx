899e9d17bcbe73f392e4346d5622f309a9d2a594
Prince Agezi .N <princeagezinweke@gmail.com>
2026-03-26 01:26:18 +0100

    fix: Hero text visiblity on Light mode.

--- components/homepage-14/HeroV14.tsx
+++ components/homepage-14/HeroV14.tsx
@@ -6,12 +6,12 @@ const HeroV14 = () => {
     <section className="relative h-[80vh] w-screen overflow-hidden lg:h-screen">
       <img src="/images/hero-flo-portfolio.jpeg" alt="hero" className="w-full blur-sm" />
       <RevealWrapper className="container absolute left-1/3 top-2/3 -translate-x-1/3 -translate-y-2/3 lg:top-1/2 lg:-translate-y-1/2">
-        <h1 className="mb-3 text-backgroundBody max-md:leading-none">
+        <h1 className="mb-3 text-secondary dark:text-backgroundBody max-md:leading-none">
           Shaping <br className="hidden md:block" />
           raw <i className="font-instrument text-inherit">footage</i> through
           <br className="hidden md:block" /> precision <i className="font-instrument text-inherit">editing</i>
         </h1>
-        <p className="max-w-3xl text-backgroundBody/70">From polished music videos to high-end TV commercials.</p>
+        <p className="max-w-3xl text-secondary/70 dark:text-backgroundBody/70">From polished music videos to high-end TV commercials.</p>
         {/* Primary CTA Button */}
         <ul className="mt-7 flex list-none justify-start lg:mt-14">
           <li className="block w-full text-center md:inline-block md:w-auto">
